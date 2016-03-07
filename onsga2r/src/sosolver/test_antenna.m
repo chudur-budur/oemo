b5ef0e6f0338af1ffc2382723984aefecbf3de83
chudur-budur <khaled.ahsan.talukder@gmail.com>
2016-03-07 11:10:50 -0500

    yo

--- onsga2r/src/sosolver/test_antenna.m
+++ onsga2r/src/sosolver/test_antenna.m
@@ -71,37 +71,63 @@ N = 12 ; % number of antenna elements
 % y = mapvars(x)
 % f = antenna(x)
 
-% testing antenna function with extreme point solver
-load pivots_antenna ;
-pv1 = pivots(1, 1:N);
-pv2 = pivots(2, 1:N);
-pv3 = pivots(3, 1:N);
+% % testing antenna function with extreme point solver
+% load pivots_antenna ;
+% pv1 = pivots(1, 1:N);
+% pv2 = pivots(2, 1:N);
+% pv3 = pivots(3, 1:N);
+% 
+% plot_pattern(5, pv1, 180, 0.5);
+% plot_pattern(6, pv2, 180, 0.5);
+% plot_pattern(7, pv3, 180, 0.5);
+% 
+% pvs = [antenna(pv1); antenna(pv2); antenna(pv3)];
+% 
+% % load antenna_permf ; % 8.2 MB
+% datafile = 'antenna_mcf.mat' ;
+% load(datafile) ;
+% fig = figure(8);
+% clf(fig);
+% scatter3(mcf(:,1)', mcf(:,2)', mcf(:,3)', 'y') % 3-obj
+% hold on ;
+% scatter3(pvs(:,1)', pvs(:,2)', pvs(:,3)', 'b');
+% xlabel('f1'); ylabel('f2'); zlabel('f3');
+% view(26, 22);
+% 
+% % save the samples into text format for gnuplot
+% [pathstr, fname, ext] = fileparts(datafile);
+% outfile = strcat('../../report/figs/data/', fname, '.out'); 
+% fp = fopen(outfile, 'w');
+% fprintf(fp, '%.3f\t%.3f\t%.3f\n', mcf');
+% fclose(fp);
+% % save the pivots as well.
+% pivotfile = '../../report/figs/data/antenna-pivots-sosolver.out' ;
+% fp = fopen(pivotfile, 'w');
+% fprintf(fp, '%10.3f\t%10.3f\t%10.3f\n', pivots(:, N+1:N+3)');
+% fclose(fp);
 
-plot_gain(5, pv1, 180, 0.5);
-plot_gain(6, pv2, 180, 0.5);
-plot_gain(7, pv3, 180, 0.5);
-
-pvs = [antenna(pv1); antenna(pv2); antenna(pv3)];
-
-% load antenna_permf ; % 8.2 MB
-datafile = 'antenna_mcf.mat' ;
-load(datafile) ;
-fig = figure(8);
-clf(fig);
-scatter3(mcf(:,1)', mcf(:,2)', mcf(:,3)', 'y') % 3-obj
-hold on ;
-scatter3(pvs(:,1)', pvs(:,2)', pvs(:,3)', 'b');
-xlabel('f1'); ylabel('f2'); zlabel('f3');
-view(26, 22);
-
-% save the samples into text format for gnuplot
-[pathstr, fname, ext] = fileparts(datafile);
-outfile = strcat('../../report/figs/data/', fname, '.out'); 
+% plot the azimuth pattern from 3 solutions, 
+% also saves the data for gnuplot
+x1 = [0.240, 0.333, 0.366, 0.237, 0.946, 0.903, ...
+        162.381, 176.390, 76.734, -160.187, -54.398, 72.076];
+[theta, db] = plot_pattern(9, x1, 180, 0.5);
+outfile = '../../report/figs/data/antenna/extreme1-pattern.out';
 fp = fopen(outfile, 'w');
-fprintf(fp, '%.3f\t%.3f\t%.3f\n', mcf');
+fprintf(fp, '%10.3f\t%10.3f\n', [theta; db]);
 fclose(fp);
-% save the pivots as well.
-pivotfile = '../../report/figs/data/antenna-pivots-sosolver.out' ;
-fp = fopen(pivotfile, 'w');
-fprintf(fp, '%10.3f\t%10.3f\t%10.3f\n', pivots(:, N+1:N+3)');
+
+x2 = [0.813, 0.441, 0.371, 0.248, 0.229, 0.843, ...
+        158.601, -157.263, 84.664, -178.959, -73.120, 160.364];
+[theta, db] = plot_pattern(10, x2, 180, 0.5);
+outfile = '../../report/figs/data/antenna/extreme2-pattern.out';
+fp = fopen(outfile, 'w');
+fprintf(fp, '%10.3f\t%10.3f\n', [theta; db]);
+fclose(fp);
+
+x3 = [0.966, 0.363, 0.373, 0.243, 0.249, 0.843, ...
+        158.384, -140.514, 84.702, 138.158, -57.385, 159.203];
+[theta, db] = plot_pattern(11, x3, 180, 0.5);
+outfile = '../../report/figs/data/antenna/solution1-pattern.out';
+fp = fopen(outfile, 'w');
+fprintf(fp, '%10.3f\t%10.3f\n', [theta; db]);
 fclose(fp);
