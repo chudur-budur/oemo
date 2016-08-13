5900c4534c1bd2a0f55f0d51d8a71e2afb98d856
chudur-budur <khaled.ahsan.talukder@gmail.com>
2016-08-13 05:21:18 -0400

    debug and mods

--- onsga2r/src/localsearch.c
+++ onsga2r/src/localsearch.c
@@ -110,6 +110,9 @@ int init_extreme_pts_hardcoded_weak(void)
 	else if(strcmp(prob_name, "dtlz1") == 0)
 		for(i = 0 ; i < nobj ; i++)
 			memcpy(extremes[i], &dtlz1_weak_extremes[i], sizeof(double) * nreal);
+	else if(strcmp(prob_name, "dtlz3") == 0)
+		for(i = 0 ; i < nobj ; i++)
+			memcpy(extremes[i], &dtlz3_weak_extremes[i], sizeof(double) * nreal);
 	else if(strcmp(prob_name, "dtlz6") == 0)
 		for(i = 0 ; i < nobj ; i++)
 			memcpy(extremes[i], &dtlz6_weak_extremes[i], sizeof(double) * nreal);
